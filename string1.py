5f0942c2cc8b28d07c0a3a4d3e87063141304fc4
Howard Post <hpost2012@gmail.com>
2020-05-08 09:29:58 -0700

    string1.py finished

--- string1.py
+++ string1.py
@@ -28,8 +28,11 @@ __author__ = "???"
 
 
 def donuts(count):
-    # your code here
-    return
+    if count < 10:
+        string = f'Number of donuts: {count}'
+    else:
+        string = 'Number of donuts: many'
+    return string
 
 
 # B. both_ends
@@ -42,8 +45,11 @@ def donuts(count):
 
 
 def both_ends(s):
-    # your code here
-    return
+    if len(s) < 2:
+        string = ''
+    else:
+        string=  s[0:2] + s[-2:]
+    return string
 
 
 # C. fix_start
@@ -59,7 +65,7 @@ def both_ends(s):
 
 def fix_start(s):
     # your code here
-    return
+    return s[0] + s[1:].replace(s[0], '*')
 
 
 # D. mix_up
@@ -74,7 +80,7 @@ def fix_start(s):
 
 def mix_up(a, b):
     # your code here
-    return
+    return b[0:2] + a[2:] + ' ' + a[0:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
